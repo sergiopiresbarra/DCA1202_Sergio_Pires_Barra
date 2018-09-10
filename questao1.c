2495387c4d4e9d100781801e5e456398f8011bd2
Sérgio Pires Barra <36992372+sergiopiresbarra@users.noreply.github.com>
2018-09-10 20:20:04 -0300

    Delete questao1.c

--- questao1.c
+++
@@ -1,6 +0,0 @@
-Questão 1:
-	Ponteiros são variáveis que foram criadas para armazenar especificamente endereço de memória. Permitem 
-diversas operaçoes, como, alocação dinâmica de memória ou atribuições de valores. A utilização de ponteiros pode
-fazer com que você gaste menos memória durante a execução de um programa e no desenvolvimento de sistemas mais
-complexos, em que é preciso utilizar tecnicas produzidas na engenharia, mais problemas que surgem podem ser
-resolvidos/simplificados com ponteiros.
