adff9f5fb4d66c742b6d1008bf9b2f4da4eded88
sergiopires201491 <sergiopires201491@gmail.com>
2018-09-30 18:23:59 -0400

    codeblocks

---
+++ Projeto01poligonos/projeto01poliCodeBlocks/main.cpp
@@ -0,0 +1,108 @@
+//main
+#include <iostream>
+#include <cmath>
+//#include "ponto.h"
+//#include "poligono.h"
+#include "retangulo.h"
+
+using namespace std;
+
+int main(){
+    Retangulo Q(0,0,4,3);
+    Q.print();
+    Q.area();
+    Q.translada(-3,4);
+    Q.area();
+    Q.rotaciona(-1,2.5,3.1415);
+    Q.area();
+
+  /*Ponto p1;
+  Poligono poli;
+  poli=4;
+  poli.vertices();
+  poli.a[0].setXY(1,1);
+  poli.a[1].setXY(1,2);
+  poli.a[2].setXY(2,2);
+  poli.a[3].setXY(2,1);
+  poli.area();
+  poli.translada(0,0);
+  //poli.rotaciona(0,0,2*3.1415);
+  poli.print();
+  Ponto p2(3,3);
+  p1.setX(3);
+  p1.setY(4);
+  p1.print();
+  p2.print();
+  cout<<"getX: "<<p1.getX()<<"\n";
+  cout<<"getY: "<<p1.getY()<<"\n";
+  p1.add(p2);
+  p1.sub(p2);
+  p1.translada(5,5);
+  p1.setXY(7,7);
+  cout<<"getXY: ";
+  p1.print();
+  cout<<"\nprint P2:";
+  p2.print();
+  cout<<"norma de p1: "<<p1.norma()<<"\n";
+  cout<<"norma de p2: "<<p2.norma()<<"\n";
+*/
+ /*
+  // criacao com copia
+  Vetor v5=v4, v6(v4);
+
+  v3 = v4+v6;
+//  v3=v4.operator+(v6);
+  cout << "v3 = ";
+  v3.print();
+  cout << endl;
+
+  v3 = v4-v6;
+  cout << "v3 = ";
+  v3.print();
+  cout << endl;
+
+  v3 = v4*4 - v6*5;
+  v3 = v4++;
+  cout << "v3 = ";
+  v3.print();
+  cout << endl;
+  */
+  /*
+  v3 = ++v4;
+  cout << "v3 = ";
+  v3.print();
+  cout << endl;
+  v3 = 4*v4;
+  cout << "v3 = ";
+  v3.print();
+  cout << endl;
+  cout << "v1.x= " << v1.getX() << endl;
+  cout << "v1.y= " << v1.getY() << endl;
+  v1.setX(3);
+  v1.setY(4);
+  cout << "v1.x= " << v1.getX() << endl;
+  cout << "v1.y= " << v1.getY() << endl;
+  cout << "tamanho = " << v1.tamanho() << endl;
+  cout << "angulo = " << v1.angulo() << endl;
+  v2 = v1.unitario();
+  cout << "unitario = " << v2.getX() <<
+          ", " << v2.getY() << endl;
+  v2.print();
+  cout << endl;
+  cout << "v2 = ";
+  v1.print();
+  cout << " + ";
+  v2.print();
+  cout << endl;
+  v2.setX(5);
+  v2.setY(11);
+  //cout << "escalar = " << escalar(v1,v2) << endl;
+  cout << "escalar = " << v1.escalar(v2) << endl;
+  v3 = v1.soma(v2);
+  v3.print();
+  cout << v3.getX() << endl;
+  cout << v3.getY() << endl;
+  v1.soma(v2).print();
+  */
+  return 0;
+}
---
+++ Projeto01poligonos/projeto01poliCodeBlocks/src/Poligono.cpp
@@ -0,0 +1,131 @@
+//poligonoCPP
+#include "poligono.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+Poligono::Poligono(int n)
+{
+    npontos=n;
+    for(int i=0; i<n; i++)
+    {
+        a[i].setX(0.0);
+        a[i].setY(0.0);
+    }
+}
+
+Poligono::~Poligono()
+{
+    cout << "chamando destrutor poligono\n";
+}
+
+void Poligono::vertices(void)
+{
+    cout<<"numero de vertices: "<<npontos<<"\n";
+}
+
+void Poligono::area(void){
+int x=npontos;
+float soma1=0,soma2=0;
+cout<<"numero de vetices definidos: "<<npontos<<"\n";
+float c[x];
+float d[x];
+/*float a[x];
+float b[x];
+for(int i=0; i<x; i++){
+    cout<<"digite as coordenadas (x,y) do vertice "<<i<<"\n";
+    cout<<"x= ";
+    cin>>a[i];
+    cout<<"\ny= ";
+    cin>>b[i];
+}
+*/
+/*for(int i=0; i<x-1; i++){
+    c[i]=a[i]*b[i+1];
+    d[i]=b[i]*a[i+1];
+}
+    c[x-1]=a[x-1]*b[0];
+    d[x-1]=b[x-1]*a[0];
+*/
+for(int i=0; i<x-1; i++){
+    c[i]=a[i].getX()*a[i+1].getY();
+    d[i]=a[i].getY()*a[i+1].getX();
+}
+    c[x-1]=a[x-1].getX()*a[0].getY();
+    d[x-1]=a[x-1].getY()*a[0].getX();
+
+for(int i=0; i<x; i++){
+    soma1=soma1+c[i];
+    soma2=soma2+d[i];
+}
+cout<<"c[x]{";
+for(int i=0; i<x; i++){
+    cout<<c[i]<<" ,";
+}
+cout<<"}\n";
+cout<<"d[y]{";
+for(int i=0; i<x; i++){
+    cout<<d[i]<<" ,";
+}
+cout<<"}\n";
+cout<<"soma1="<<soma1;
+cout<<"soma2="<<soma2;
+float sub=soma1-soma2;
+float area=abs(sub/2);
+    cout<<"Area total e': "<<area<<"\n";
+}
+
+void Poligono::translada(float A, float B){
+   int x=npontos;
+    for(int i=0; i<x; i++){
+       a[i].setX(a[i].getX()+A);
+       a[i].setY(a[i].getY()+B);
+    }
+    cout<<"nova posicao do poligono:";
+    for(int i=0; i<x; i++){
+    cout<<"("<<a[i].getX()<<", "<<a[i].getY()<<") ";
+    }
+    cout<<"\n";
+    }
+
+void Poligono::rotaciona(float X1, float Y1, float graus){
+
+//novoX = centroX + (pontoX - centroX) * Math.cos(angulo) - (pontoY - centroY) * Math.sin(angulo);
+//novoY = centroY + (pontoX - centroX) * Math.sin(angulo) + (pontoY - centroY) * Math.cos(angulo);
+
+//xf = (xo - xr) * cos (@) - (yo - yr) * sin (@) + xr
+//yf = (yo - yr) * cos (@) + (xo - xr) * sin (@) + yr
+//(xo,yo) = Ponto que você deseja rotacionar
+//(xr,yr) = Ponto em que você vai rotacionar o ponto acima (no seu caso o centro do retangulo)
+//(xf,yf) = O novo local do ponto rotacionado
+//@ = Angulo de rotação
+
+
+   int x=npontos;
+    for(int i=0; i<x; i++){
+             a[i].setX((a[i].getX() - X1) * cos(graus) - (a[i].getY() - Y1) * sin(graus) + X1);
+             a[i].setY((a[i].getY() - Y1) * cos(graus) + (a[i].getX() - X1) * sin(graus) + Y1);
+            //a[i].setX( X1 + (a[i].getX() - X1) * cos(graus) - (a[i].getY() - Y1) * sin(graus));
+            //a[i].setY( Y1 + (a[i].getX() - X1) * sin(graus) + (a[i].getY() - Y1) * cos(graus));
+    }
+  cout<<"nova posicao do poligono:";
+    for(int i=0; i<x; i++){
+    cout<<"("<<a[i].getX()<<", "<<a[i].getY()<<") ";
+    }
+    cout<<"\n";
+}
+
+void Poligono::print(void){
+    cout<<"npontos="<<npontos<<"\n";
+    int x=npontos;
+for(int i=0; i<x; i++){
+        if(i<x-1){
+    cout<<"("<<a[i].getX()<<", "<<a[i].getY()<<")->";
+        }
+        if(i==x-1){
+         cout<<"("<<a[i].getX()<<", "<<a[i].getY()<<")";
+        }
+    }
+    cout<<"\n";
+
+}
